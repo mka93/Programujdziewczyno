456ffa9fd69b4de595d6c25907a0f2b41f4ca953
mka93 <karuska@onet.eu>
2017-10-21 16:07:04 +0200

    version 1.6

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
- for (var i = 0; i < menuList.length; i++) {
+    for (var i = 0; i < menuList.length; i++) {
         menuList[i].addEventListener("mouseout", function () {
             //console.log("hurra");
             var dropdown = this.querySelector(".dropdown");
@@ -28,7 +28,39 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
     }
-    
-    
-    
+
+    var more = document.querySelectorAll(".more");
+
+    for (var j = 0; j < more.length; j++) {
+        more[j].style.display = "none";
+    }
+
+    var buttons = document.querySelectorAll(".read-more");
+    //console.log(buttons);
+
+    for (var k = 0; k < buttons.length; k++) {
+        buttons[k].addEventListener('click', function () {
+            //console.log('działa'); // To powinnaś zobaczyć w konsoli po kliknięciu w pierwszy przycisk
+            var textArea = this.previousElementSibling;
+
+            if (textArea.style.display === 'none' || textArea.style.display === '') {
+                textArea.style.display = 'block';
+                this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+            } else {
+                textArea.style.display = 'none';
+                this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
+            }
+        });
+    }
+
+    var edge = document.querySelector(".navbar");
+    //edge.style.borderBottom = "3px solid black";    
+    window.addEventListener('scroll', function () {
+        //console.log('działa przy scrollu');
+        edge.style.borderBottom = "3px solid #7f7f7f";
+    });
+
+
+
+
 });
