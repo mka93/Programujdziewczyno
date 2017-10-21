95d5408c5c51fd880a48b3b250dbc67ce60fffa2
mka93 <karuska@onet.eu>
2017-10-21 15:37:10 +0200

    version 1.5

--- js/app.js
+++ js/app.js
@@ -1,6 +1,34 @@
 //DOM (document object model) api
 'use strict';
 
-document.addEventListener('DOMContentLoaded', function() {
-      // tutaj będziemy umieszczać kod ze wszystkich zadań
-    });
+document.addEventListener('DOMContentLoaded', function () {
+    // tutaj będziemy umieszczać kod ze wszystkich zadań
+
+    var menuList = document.querySelectorAll('.nav-list > li'); //szukanie tylko dzieci! żadnych wnuczków
+    console.log(menuList); //mamy tutaj tablice!
+
+    for (var i = 0; i < menuList.length; i++) {
+        menuList[i].addEventListener("mouseover", function () {
+            //console.log("hurra");
+            var dropdown = this.querySelector(".dropdown");
+            //console.log(dropdown);
+            if (dropdown != null) {
+                dropdown.style.display = "block";
+            }
+        });
+    }
+
+ for (var i = 0; i < menuList.length; i++) {
+        menuList[i].addEventListener("mouseout", function () {
+            //console.log("hurra");
+            var dropdown = this.querySelector(".dropdown");
+            //console.log(dropdown);
+            if (dropdown != null) {
+                dropdown.style.display = "none";
+            }
+        });
+    }
+    
+    
+    
+});
