c90be319e1b57b05a901cfc29263eb040b7aed3d
mka93 <karuska@onet.eu>
2017-10-21 14:46:13 +0200

    version 1.4

---
+++ js/app.js
@@ -0,0 +1,63 @@
+/*var myNumber = 2.2;
+var myNumber2 = 8;
+
+var myText1 = "lalaland";
+
+console.log(myText1);
+console.log(myNumber + myNumber2);
+
+var prawda = true;
+var falsz = false;
+
+//Tablice
+var tablica = [1, 2, 3];
+var tablica2 = ["owoc", "owoc2"]
+
+for (i = 0; i < tablica.length; i++) {
+    tablica[i] = tablica[i] + 2;
+}
+
+console.log(tablica);
+console.log(myNumber);
+
+//funkcja
+
+function myFunction() {
+    myNumber = 3;
+    console.log(myNumber);
+}
+
+myFunction();
+
+if (myNumber > 2.2) {
+    console.log(prawda);
+} else {
+    console.log(falsz);
+}
+*/
+/*
+var mojeImie = "Monika";
+
+var imiona = ["Asia", "Magda", "Eliza", "Agnieszka"];
+
+console.log(imiona[1]);
+
+var liczba = 1;
+var liczba2 = 10;
+
+if (liczba > liczba2) {
+    console.log("hurra!")
+} else {
+    console.log("nie tym razem")
+}
+
+for (i = 0; i < imiona.length; i++) {
+    console.log(imiona[i]);
+}
+
+//var tab = document.getElementsByClassName('row');
+//console.log(tab);
+*/
+
+//DOM (document object model) api
+
